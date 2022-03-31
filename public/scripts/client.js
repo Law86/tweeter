4f6e5d314cf4a38de728f768c9e0b4a2598d8e3f
Michael Law <mikelaw86@hotmail.com>
2022-03-30 21:00:09 -0400

    Add form submission functionality with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
 const data = [
   {
     "user": {
@@ -30,15 +32,47 @@ const data = [
 ]
 
 const renderTweets = function(tweets) {
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
+ tweets.forEach(element => {
+  $('.tweets-container').append(createTweetElement(element))
+ });
+
 }
 
+$('.new-tweet-form').submit((event) => {
+  event.preventDefault();
+  let data = $('.new-tweet-form').serialize();
+  console.log(data)
+  $.post("/tweets", data)
+});
+
+
 const createTweetElement = function(tweet) {
-let $tweet = /* Your code for creating the tweet element */
-// ...
-return $tweet;
+let $tweet = `
+<article class="tweet">  
+  <header class="post-tweet-header">       
+  <img class="post-user-avatar" src="${tweet.user.avatars}" width="30" height="30">
+  <div class="post-user-name">${tweet.user.name}</div>
+  <div class="post-user-handle">${tweet.user.handle}</div>
+  </header>
+  <div>
+  <p class="posted-tweet-text">
+  ${tweet.content.text}
+  </p>
+  </div>
+  <footer class="submit-count">
+    <output class="tweet-time" for="tweet-text">"${tweet.created_at}"</output>
+    <div class="posted-tweet-buttons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </div>
+</form>
+</article>
+`
+  return $tweet;
 }
 
 renderTweets(data);
+
+});
